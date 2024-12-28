507b7322501e8a58ee7733fa46df3a362da2133c
Aylén Orellana <ayluorellana@gmail.com>
2024-12-28 20:17:55 +0100

    :sparkles: add Image component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import Column from "./Column";
-import Row from "./Row";
+import { Column } from "./Column";
+import { Row } from "./Row";
 export const Header = () => {
   return (
     <Column>
       <Row>
-        <h1 style={{ margin: 0, padding: 0 }}> Ecommerce App</h1>
+        <h1> Ecommerce App</h1>
         <Row>
           <button>cart</button>
           <button>Sign Up</button>
